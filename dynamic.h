ae16063ffa96e075e4fd402131fd8f447303794b
Henrique Rieger <henriquerieger2001@gmail.com>
2022-01-26 17:07:26 -0300

    [add] macros for pop and index operations

--- dynamic.h
+++ dynamic.h
@@ -1,6 +1,7 @@
 # ifndef __DYNAMIC_H__
 # define __DYNAMIC_H__
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 
@@ -21,23 +22,101 @@
 // Return the real number of blocks allocated by the array
 # define realSize(a) (1 << a->size)
 
-// Insert element in array
+// Return the length of the array
+# define len(a) (a->len)
+
+// Return the array itself
+# define data(a) (a->data)
+
+// Insert element in end of array
 # define insert(a, i, type_t) ({ \
-    if ((realSize(a)) > (a->len)) \
-    { \
-        a->data[a->len] = i; \
-        a->len++; \
-    } \
-    else \
+    if ((realSize(a)) <= (a->len)) \
     { \
         void *tmp; \
-        tmp = malloc(2*((realSize(a)) * sizeof(type_t))); \
+        tmp = malloc(((realSize(a)) * sizeof(type_t)) << 1); \
         memcpy(tmp, a->data, (realSize(a) * sizeof(type_t))); \
+        free(a->data); \
         a->data = tmp; \
         a->size++; \
-        a->data[a->len] = i; \
-        a->len++; \
+    } \
+    a->data[a->len] = i; \
+    a->len++; \
+})
+
+// Insert element in specific index of array
+# define insertIn(a, i, pos, type_t) ({ \
+    if ((pos) > (a->len) || ((pos) < 0)) \
+    { \
+        fprintf(stderr, "Error: Index %d is out of bounds.\n", (pos)); \
+        exit(1); \
+    } \
+\
+    if ((realSize(a)) <= (a->len)) \
+    { \
+        void *tmp; \
+        tmp = malloc(((realSize(a)) * sizeof(type_t)) << 1); \
+        memcpy(tmp, a->data, (realSize(a) * sizeof(type_t))); \
+        free(a->data); \
+        a->data = tmp; \
+        a->size++; \
+    } \
+    memcpy(a->data+pos+1, a->data+pos, ((a->len)-(pos)-1) * sizeof(type_t)); \
+    a->data[pos] = i; \
+    a->len++; \
+})
+
+// Return element in 'dest' and delete it from the array
+# define pop(dest, a, i, type_t) ({ \
+    for (int indexOfPop = 0; indexOfPop < a->len; indexOfPop++) { \
+        if ((a->data[indexOfPop]) == i) \
+        { \
+            if ((dest) != NULL) \
+                *dest = a->data[indexOfPop]; \
+            memcpy(a->data+indexOfPop, a->data+indexOfPop+1, ((a->len) - (indexOfPop) - 1) * sizeof(type_t)); \
+            a->len--; \
+            break; \
+        } \
+    } \
+\
+    if ((realSize(a) >> 1) > (a->len)) \
+    { \
+        void *tmp; \
+        tmp = malloc(((realSize(a)) * sizeof(type_t)) >> 1); \
+        memcpy(tmp, a->data, (len(a) * sizeof(type_t))); \
+        free(a->data); \
+        a->data = tmp; \
+        a->size--; \
     } \
 })
+
+// Return element in 'dest' and delete it from the array
+# define popIn(dest, a, pos, type_t) ({ \
+    if ((pos) > (a->len) || ((pos) < 0)) \
+    { \
+        fprintf(stderr, "Error: Index %d is out of bounds.\n", (pos)); \
+        exit(1); \
+    } \
+\
+    if ((dest) != NULL) \
+        *dest = a->data[pos]; \
+    memcpy(a->data+pos, a->data+pos+1, ((a->len) - (pos) - 1) * sizeof(type_t)); \
+    a->len--; \
+\
+    if ((realSize(a) >> 1) > (a->len)) \
+    { \
+        void *tmp; \
+        tmp = malloc(((realSize(a)) * sizeof(type_t)) >> 1); \
+        memcpy(tmp, a->data, (len(a) * sizeof(type_t))); \
+        free(a->data); \
+        a->data = tmp; \
+        a->size--; \
+    } \
+})
+
+createArrayType(int);
+createArrayType(char);
+createArrayType(float);
+createArrayType(long);
+createArrayType(double);
 
 # endif
--- test.c
+++ test.c
@@ -2,24 +2,60 @@
 
 # include "dynamic.h"
 
-int main() {
-    createArrayType(int);
+void printArray(arrayType(int) * array) {
+    printf("Size: %lld, Length: %lld, Array: [", realSize(array), len(array));
+    for (int i = 0; i < len(array); i++)
+    {
+        printf("%d", data(array)[i]);
+        if (i != len(array) - 1)
+            printf(", ");
+    }
+    printf("]\n");
+}
 
+int main() {
     arrayType(int) *array;
     NewArray(array, int);
 
     for (int i = 0; i < 20; i++)
     {
-    
         insert(array, i, int);
-
-        printf("Size: %lld, Length: %lld, Array: [", realSize(array), array->len);
-        for (int i = 0; i < array->len; i++)
-        {
-            printf("%d", array->data[i]);
-            if (i != array->len - 1)
-                printf(", ");
-        }
-        printf("]\n");
+        printArray(array);
     }
+
+    int item;
+    pop(&item, array, 6, int);
+    printArray(array);
+
+    insertIn(array, 78, 12, int);
+    printArray(array);
+
+    popIn(&item, array, 5, int);
+    printArray(array);
+
+    pop(&item, array, 78, int);
+    printArray(array);
+    printf("%d\n", item);
+
+    while (len(array) > 0)
+    {
+        popIn(&item, array, 0, int);
+        printArray(array);
+    }
+
+    insert(array, 1, int);
+    insert(array, 1, int);
+    insert(array, 1, int);
+    printArray(array);
+
+    pop(&item, array, 1, int);
+    printArray(array);
+
+    int one = 1;
+    pop(&item, array, one, int);
+    printArray(array);
+
+    int zero = 0;
+    popIn(&item, array, zero, int);
+    printArray(array);
 }
